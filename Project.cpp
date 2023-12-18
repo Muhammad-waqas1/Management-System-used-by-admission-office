2a0d85f0d66f3653aed8019c62986c21b7e73006
Muhammad Waqas <149311000+Muhammad-waqas1@users.noreply.github.com>
2023-12-18 21:25:39 +0500

    Update Project.cpp
    Modified editStudentInformation() function
    It still have some problem will fix it later :(

--- Project.cpp
+++ Project.cpp
@@ -15,7 +15,8 @@
 using namespace std;
 
 // Define a structure to represent a student
-struct Student {
+struct Student
+{
     int rollNumber;
     string name;
     int age;
@@ -25,38 +26,41 @@ struct Student {
 
 // Function prototypes
 int displayMenu(int);
-void addStudent(Student& student);
+void addStudent(Student &student);
 void showStudentList();
-void editStudentInformation();
+void editStudentInformation(Student &student);
 void deleteStudent();
 void registerStudentInCourse();
 void enterMarks();
 void printGrades();
 
-int main() {
-    
+int main()
+{
+
     int choice;
-    do{
+    do
+    {
         Student new_student;
-        choice=displayMenu(choice);
-        if(choice==1)
+        choice = displayMenu(choice);
+        if (choice == 1)
             addStudent(new_student);
-        else if(choice==2)
+        else if (choice == 2)
             showStudentList();
-        else if(choice==3)
-            editStudentInformation();
-        else if(choice==4)
+        else if (choice == 3)
+            editStudentInformation(new_student);
+        else if (choice == 4)
             deleteStudent();
-        else if(choice==5)
+        else if (choice == 5)
             registerStudentInCourse();
-        else if(choice==7)
+        else if (choice == 7)
             printGrades();
-    }while(choice !=8);
+    } while (choice != 8);
 
     return 0;
 }
 
-int displayMenu(int choice1) {
+int displayMenu(int choice1)
+{
     cout << "\n===== University Admission Office Management System =====\n";
     cout << "1. Add Student\n";
     cout << "2. Show List of Students\n";
@@ -66,44 +70,50 @@ int displayMenu(int choice1) {
     cout << "6. Enter Marks of a Student or Complete Class\n";
     cout << "7. Print Grades of Students\n";
     cout << "8. Exit\n";
-    cin>>choice1;
-    while (choice1 < 1 || choice1 > 8) {
+    cin >> choice1;
+    while (choice1 < 1 || choice1 > 8)
+    {
         cout << "Invalid choice. Please enter a number between 1 and 8: ";
         cin >> choice1;
     }
     return choice1;
 }
 
-void addStudent(Student& student) {
+void addStudent(Student &student)
+{
     cout << "Enter Roll Number: ";
     cin >> student.rollNumber;
     cout << "Enter Name: ";
-    cin.ignore(); 
+    cin.ignore();
     getline(cin, student.name);
     cout << "Enter Age: ";
     cin >> student.age;
     cout << "Enter Course: ";
-    cin.ignore();  
+    cin.ignore();
     getline(cin, student.course);
     cout << "Enter Marks: ";
     cin >> student.marks;
 
     ofstream outputFile("student_data.txt", ios::app);
-    if (outputFile.is_open()) {
+    if (outputFile.is_open())
+    {
         outputFile << "Roll Number: " << student.rollNumber << endl;
         outputFile << "Name: " << student.name << endl;
         outputFile << "Age: " << student.age << endl;
         outputFile << "Course: " << student.course << endl;
         outputFile << "Marks: " << student.marks << endl;
-        outputFile << "-------------------------\n";  // Separator for multiple students data
+        outputFile << "-------------------------\n"; // Separator for multiple students data
         outputFile.close();
         cout << "Student data has been saved to student_data.txt.\n";
-    } else {
+    }
+    else
+    {
         cout << "Error opening file for writing!\n";
     }
 }
 
-void showStudentList() {
+void showStudentList()
+{
     ifstream inputFile("student_data.txt");
     if (inputFile.is_open())
     {
@@ -117,48 +127,77 @@ void showStudentList() {
     }
     else
     {
-        cout << "Error opening file for reading!" << endl;;
+        cout << "Error opening file for reading!" << endl;
+        ;
     }
-
 }
 
-void editStudentInformation(Student& student) {
+void editStudentInformation(Student &student)
+{
+    cout << "::::::::Student Information Editing Mode::::::::" << endl;
     cout << "Enter the Roll Number of the student you want to edit: ";
     int rollNumber;
     cin >> rollNumber;
-    ifstream inputFile("student_data.txt");
-    
-    if (inputFile.is_open()) {
-        while (inputFile >> student.rollNumber >> student.name >> student.age >> student.course >> student.marks) {
-            if (student.rollNumber == rollNumber) {
-                inputFile.close();
-                // return true;  // Found the student with the given roll number
+    // ifstream inputFile("student_data.txt");
+    fstream file("student_data.txt", ios::in | ios::out);
+
+    if (file.is_open())
+    {
+        while (file >> student.rollNumber >> student.name >> student.age >> student.course >> student.marks)
+        {
+            if (student.rollNumber == rollNumber)
+            {
+                // Modify the student information if found
+                cout << "Enter updated information:\n";
+                cout << "Name: ";
+                cin >> student.name;
+                cout << "Age: ";
+                cin >> student.age;
+                cout << "Course: ";
+                cin >> student.course;
+                cout << "Marks: ";
+                cin >> student.marks;
+
+                // Move the file cursor back to the beginning of the current student's record
+                // file.seekg(file.tellg());
+                file.seekp(file.tellp());
+                // Write the modified data
+                file << student.rollNumber << " " << student.name << " " << student.age << " " << student.course << " " << student.marks << endl;
+                cout << "Successfully edited\n";
+                break;
             }
         }
-
-        inputFile.close();
-    } else {
-        cout << "Error opening file for reading!\n";
+        if (!file.eof() && file.fail())
+        {
+            cout << "Error reading from file!\n";
+            // inputFile.close();
+            file.close();
+        }
+        else
+            cout << "Error opening file for reading!\n";
     }
-    // return false;
 }
 
-void deleteStudent() {
+void deleteStudent()
+{
     // Implementation for deleting a student
     // ...
 }
 
-void registerStudentInCourse() {
+void registerStudentInCourse()
+{
     // Implementation for registering a student in a course
     // ...
 }
 
-void enterMarks() {
+void enterMarks()
+{
     // Implementation for entering marks of a student or complete class
     // ...
 }
 
-void printGrades() {
+void printGrades()
+{
     // Implementation for printing grades of students
     // ...
 }
