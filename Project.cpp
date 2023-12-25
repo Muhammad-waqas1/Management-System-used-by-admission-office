9c712b3705d0ddd61f6a046272c444fa4fdfc7fc
Muhammad Waqas <149311000+Muhammad-waqas1@users.noreply.github.com>
2023-12-25 14:34:41 +0500

    Update Project.cpp
    Updated rest of the functions 
    but
    still there is a little bug 👎 
    :(
    :(
    :(
    :(

--- Project.cpp
+++ Project.cpp
@@ -1,22 +1,9 @@
-// In this project, you have to create Management System used by admission office of the university. This
-// software is used by office to enter info about new students as well as used by teachers to enter marks of the students for a given exam and course. The program has features like
-//  Add Student
-//  Show list Student
-//  Edit Student Information
-//  Delete Student
-//  Register student in a course
-//  Enter marks of the student or complete class
-//  Print Grades of students
-// Note: use arrays, structures, files, loops 
-
 #include <iostream>
-#include <string>
 #include <fstream>
+#include <string>
 using namespace std;
 
-// Define a structure to represent a student
-struct Student
-{
+struct Student {
     int rollNumber;
     string name;
     int age;
@@ -24,180 +11,304 @@ struct Student
     double marks;
 };
 
-// Function prototypes
-int displayMenu(int);
-void addStudent(Student &student);
-void showStudentList();
-void editStudentInformation(Student &student);
-void deleteStudent();
-void registerStudentInCourse();
-void enterMarks();
-void printGrades();
+void addStudent(Student *&students, int &numStudents);
+void showStudentList(const Student *students, int numStudents);
+void editStudentInformation(Student *students, int numStudents);
+void deleteStudent(Student *&students, int &numStudents);
+void registerStudentInCourse(Student *students, int numStudents);
+void enterMarks(Student *students, int numStudents);
+void printGrades(const Student *students, int numStudents);
+string Calculating_Grades(double marks);
 
-int main()
-{
-
+int main() {
+    Student *students = nullptr;
+    int numStudents = 0;
     int choice;
-    do
-    {
-        Student new_student;
-        choice = displayMenu(choice);
-        if (choice == 1)
-            addStudent(new_student);
-        else if (choice == 2)
-            showStudentList();
-        else if (choice == 3)
-            editStudentInformation(new_student);
-        else if (choice == 4)
-            deleteStudent();
-        else if (choice == 5)
-            registerStudentInCourse();
-        else if (choice == 7)
-            printGrades();
+
+    do {
+        cout << "\n===== University Management System =====\n";
+        cout << "1. Add Student\n";
+        cout << "2. Show List of Students\n";
+        cout << "3. Edit Student Information\n";
+        cout << "4. Delete Student\n";
+        cout << "5. Register Student in a Course\n";
+        cout << "6. Enter Marks of a Student or Complete Class\n";
+        cout << "7. Print Grades of Students\n";
+        cout << "8. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                addStudent(students, numStudents);
+                break;
+            case 2:
+                showStudentList(students, numStudents);
+                break;
+            case 3:
+                editStudentInformation(students, numStudents);
+                break;
+            case 4:
+                deleteStudent(students, numStudents);
+                break;
+            case 5:
+                addStudent(students, numStudents);
+                // registerStudentInCourse(students, numStudents);
+                break;
+            case 6:
+                enterMarks(students, numStudents);
+                break;
+            case 7:
+                printGrades(students, numStudents);
+                break;
+            case 8:
+                cout << "Exiting the program.\n";
+                break;
+            default:
+                cout << "Invalid choice. Please enter a number between 1 and 8.\n";
+        }
+
     } while (choice != 8);
+
+    // Deallocate memory before exiting
+    delete[] students;
 
     return 0;
 }
 
-int displayMenu(int choice1)
-{
-    cout << "\n===== University Admission Office Management System =====\n";
-    cout << "1. Add Student\n";
-    cout << "2. Show List of Students\n";
-    cout << "3. Edit Student Information\n";
-    cout << "4. Delete Student\n";
-    cout << "5. Register Student in a Course\n";
-    cout << "6. Enter Marks of a Student or Complete Class\n";
-    cout << "7. Print Grades of Students\n";
-    cout << "8. Exit\n";
-    cin >> choice1;
-    while (choice1 < 1 || choice1 > 8)
-    {
-        cout << "Invalid choice. Please enter a number between 1 and 8: ";
-        cin >> choice1;
-    }
-    return choice1;
-}
+void addStudent(Student *&students, int &numStudents) {
+    // Allocate memory for a new student
+    Student newStudent;
 
-void addStudent(Student &student)
-{
     cout << "Enter Roll Number: ";
-    cin >> student.rollNumber;
+    cin >> newStudent.rollNumber;
     cout << "Enter Name: ";
     cin.ignore();
-    getline(cin, student.name);
+    getline(cin, newStudent.name);
     cout << "Enter Age: ";
-    cin >> student.age;
+    cin >> newStudent.age;
     cout << "Enter Course: ";
     cin.ignore();
-    getline(cin, student.course);
+    getline(cin, newStudent.course);
     cout << "Enter Marks: ";
-    cin >> student.marks;
+    cin >> newStudent.marks;
 
-    ofstream outputFile("student_data.txt", ios::app);
-    if (outputFile.is_open())
-    {
-        outputFile << "Roll Number: " << student.rollNumber << endl;
-        outputFile << "Name: " << student.name << endl;
-        outputFile << "Age: " << student.age << endl;
-        outputFile << "Course: " << student.course << endl;
-        outputFile << "Marks: " << student.marks << endl;
-        outputFile << "-------------------------\n"; // Separator for multiple students data
-        outputFile.close();
-        cout << "Student data has been saved to student_data.txt.\n";
+    // Resize the array to accommodate the new student
+    Student *temp = new Student[numStudents + 1];
+    for (int i = 0; i < numStudents; ++i) {
+        temp[i] = students[i];
     }
-    else
-    {
+
+    // Add the new student to the array
+    temp[numStudents] = newStudent;
+
+    // Deallocate the old array
+    delete[] students;
+
+    // Update the pointer to the new array
+    students = temp;
+
+    numStudents++;
+
+    // Save to file
+    ofstream outputFile("student404_data.txt", ios::app);
+    if (outputFile.is_open()) {
+        outputFile << newStudent.rollNumber << " "
+                   << newStudent.name << " "
+                   << newStudent.age << " "
+                   << newStudent.course << " "
+                   << newStudent.marks << endl;
+
+        outputFile.close();
+        cout << "Student data has been saved to student404_data.txt.\n";
+    } else {
         cout << "Error opening file for writing!\n";
     }
 }
 
-void showStudentList()
-{
-    ifstream inputFile("student_data.txt");
-    if (inputFile.is_open())
-    {
-        cout << "\nContents of user_data.txt:\n";
-        string line;
-        while (getline(inputFile, line))
-        {
-            cout << line << endl;
+void showStudentList(const Student *students, int numStudents) {
+    ifstream inputFile("student404_data.txt");
+    if (inputFile.is_open()) {
+        cout << "\nList of Students:\n";
+        for (int i = 0; i < numStudents; ++i) {
+            cout << "Roll Number: " << students[i].rollNumber << endl;
+            cout << "Name: " << students[i].name << endl;
+            cout << "Age: " << students[i].age << endl;
+            cout << "Course: " << students[i].course << endl;
+            cout << "Marks: " << students[i].marks << endl;
+            cout << "-------------------------\n";
         }
         inputFile.close();
-    }
-    else
-    {
-        cout << "Error opening file for reading!" << endl;
-        ;
+    } else {
+        cout << "Error opening file for reading!\n";
     }
 }
 
-void editStudentInformation(Student &student)
-{
-    cout << "::::::::Student Information Editing Mode::::::::" << endl;
+void editStudentInformation(Student *students, int numStudents) {
     cout << "Enter the Roll Number of the student you want to edit: ";
     int rollNumber;
     cin >> rollNumber;
-    // ifstream inputFile("student_data.txt");
-    fstream file("student_data.txt", ios::in | ios::out);
 
-    if (file.is_open())
-    {
-        while (file >> student.rollNumber >> student.name >> student.age >> student.course >> student.marks)
-        {
-            if (student.rollNumber == rollNumber)
-            {
-                // Modify the student information if found
-                cout << "Enter updated information:\n";
-                cout << "Name: ";
-                cin >> student.name;
-                cout << "Age: ";
-                cin >> student.age;
-                cout << "Course: ";
-                cin >> student.course;
-                cout << "Marks: ";
-                cin >> student.marks;
+    bool found = false;
 
-                // Move the file cursor back to the beginning of the current student's record
-                // file.seekg(file.tellg());
-                file.seekp(file.tellp());
-                // Write the modified data
-                file << student.rollNumber << " " << student.name << " " << student.age << " " << student.course << " " << student.marks << endl;
-                cout << "Successfully edited\n";
-                break;
+    for (int i = 0; i < numStudents; ++i) {
+        if (students[i].rollNumber == rollNumber) {
+            cout << "Enter updated information:\n";
+            cout << "Name: ";
+            cin.ignore();
+            getline(cin, students[i].name);
+            cout << "Age: ";
+            cin >> students[i].age;
+            cout << "Course: ";
+            cin.ignore();
+            getline(cin, students[i].course);
+            cout << "Marks: ";
+            cin >> students[i].marks;
+
+            found = true;
+            break;
+        }
+    }
+
+    if (found) {
+        // Save to file
+        ofstream outputFile("student404_data.txt");
+        if (outputFile.is_open()) {
+            for (int i = 0; i < numStudents; ++i) {
+                outputFile << students[i].rollNumber << " "
+                           << students[i].name << " "
+                           << students[i].age << " "
+                           << students[i].course << " "
+                           << students[i].marks << endl;
             }
+            outputFile.close();
+            cout << "Successfully edited\n";
+        } else {
+            cout << "Error opening file for writing!\n";
         }
-        if (!file.eof() && file.fail())
-        {
-            cout << "Error reading from file!\n";
-            // inputFile.close();
-            file.close();
-        }
-        else
-            cout << "Error opening file for reading!\n";
+    } else {
+        cout << "Student with Roll Number " << rollNumber << " not found.\n";
     }
 }
 
-void deleteStudent()
-{
-    // Implementation for deleting a student
-    // ...
+void deleteStudent(Student *&students, int &numStudents) {
+    cout << "Enter the Roll Number of the student you want to delete: ";
+    int rollNumber;
+    cin >> rollNumber;
+
+    bool found = false;
+
+    for (int i = 0; i < numStudents; ++i) {
+        if (students[i].rollNumber == rollNumber) {
+            found = true;
+            // Shift the remaining elements to fill the gap
+            for (int j = i; j < numStudents - 1; ++j) {
+                students[j] = students[j + 1];
+            }
+            numStudents--;
+
+            // Resize the array to accommodate the removed student
+            Student *temp = new Student[numStudents];
+            for (int k = 0; k < numStudents; ++k) {
+                temp[k] = students[k];
+            }
+
+            // Deallocate the old array
+            delete[] students;
+
+            // Update the pointer to the new array
+            students = temp;
+
+            break;
+        }
+    }
+
+    if (found) {
+        // Save to file
+        ofstream outputFile("student404_data.txt");
+        if (outputFile.is_open()) {
+            for (int i = 0; i < numStudents; ++i) {
+                outputFile << students[i].rollNumber << " "
+                           << students[i].name << " "
+                           << students[i].age << " "
+                           << students[i].course << " "
+                           << students[i].marks << endl;
+            }
+            outputFile.close();
+            cout << "Student with Roll Number " << rollNumber << " has been deleted.\n";
+        } else {
+            cout << "Error opening file for writing!\n";
+        }
+    } else {
+        cout << "Student with Roll Number " << rollNumber << " not found.\n";
+    }
 }
 
-void registerStudentInCourse()
-{
+void registerStudentInCourse(Student *students, int numStudents) {
     // Implementation for registering a student in a course
     // ...
 }
 
-void enterMarks()
-{
-    // Implementation for entering marks of a student or complete class
-    // ...
+void enterMarks(Student *students, int numStudents) {
+    cout << "Enter the Roll Number of the student you want to Enter Marks of: ";
+    int rollNumber;
+    cin >> rollNumber;
+
+    bool found = false;
+
+    for (int i = 0; i < numStudents; ++i) {
+        if (students[i].rollNumber == rollNumber) {
+            cout << "Enter updated Marks:\n";
+            cout << "Marks: ";
+            cin >> students[i].marks;
+            found = true;
+            break;
+        }
+    }
+
+    if (found) {
+        // Save to file
+        ofstream outputFile("student404_data.txt");
+        if (outputFile.is_open()) {
+            for (int i = 0; i < numStudents; ++i) {
+                outputFile<< students[i].marks << endl;
+            }
+            outputFile.close();
+            cout << "Successfully edited\n";
+        } else {
+            cout << "Error opening file for writing!\n";
+        }
+    } else {
+        cout << "Student with Roll Number " << rollNumber << " not found.\n";
+    }
 }
 
-void printGrades()
-{
-    // Implementation for printing grades of students
-    // ...
+void printGrades(const Student *students, int numStudents) {
+    ifstream inputFile("student404_data.txt");
+    if (inputFile.is_open()) {
+        cout << "\nList of Students:\n";
+        for (int i = 0; i < numStudents; ++i) {
+            cout << "Roll Number: " << students[i].rollNumber << endl;
+            cout << "Course: " << students[i].course << endl;
+            cout << "Grades: " << Calculating_Grades(students[i].marks)<< endl;
+            cout << "-------------------------\n";
+        }
+        inputFile.close();
+    } else {
+        cout << "Error opening file for reading!\n";
+    }
+}
+string Calculating_Grades(double a){
+    if(a>90)
+        return "A+";
+    else if(a>80)
+        return "A";
+    else if(a>70)
+        return "B";
+    else if(a>60)
+        return "C";
+    else if(a>50)
+        return "D";
+    else
+        return "E";
 }
